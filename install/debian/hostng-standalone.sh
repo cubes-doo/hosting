3d33e41f7b222dac48f8e1bb723d3a1fa258a009
Aleksandar Dimic <aleksandar.dimic@cubes.rs>
2021-05-31 11:25:42 +0200

    Dodata php7.4-mysql ekstenzija zbog phpmyadmin-a

--- install/debian/hostng-standalone.sh
+++ install/debian/hostng-standalone.sh
@@ -17,6 +17,7 @@ apt-get -y install php7.4-odbc php7.4-opcache php7.4-phpdbg php7.4-psr php7.4-re
 apt-get -y install php7.4-soap php7.4-solr php7.4-sqlite3 php7.4-ssh2 php7.4-uploadprogress 
 apt-get -y install php7.4-uuid php7.4-xdebug php7.4-xml php7.4-xmlrpc php7.4-xsl php7.4-yaml php7.4-zip
 apt-get -y install php7.4-propro php7.4-raphf
+apt-get -y install php7.4-mysql
 wget -O /etc/php/7.4/fpm/pool.d/www.conf https://raw.githubusercontent.com/cubes-doo/hosting/master/configs/php/fpm/pool.d/www.conf
 systemctl enable php7.4-fpm
 systemctl restart php7.4-fpm
