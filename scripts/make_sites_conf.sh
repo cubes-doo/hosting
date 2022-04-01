9032eea59d98eff00dff65a94c850fc615321416
mil4ndjcubes <84564050+mil4ndjcubes@users.noreply.github.com>
2022-04-01 12:51:53 +0200

    Kreirana skripta za kreiranje sajtova

---
+++ scripts/make_sites_conf.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+DOMAIN="mydomain.com"
+
+
+mkdir -p /var/www/$DOMAIN
+mkdir -p /var/www/$DOMAIN/www-root && mkdir -p /var/www/$DOMAIN/logs
+touch /var/www/$DOMAIN/logs/.borgignore
+mkdir -p /var/www/$DOMAIN/www-root/piblic
+chown -R cubes: /var/www/$DOMAIN/www-root
+
+touch /etc/nginx/sites-available/$DOMAIN.conf
+wget -O /etc/nginx/sites-available/$DOMAIN.conf https://raw.githubusercontent.com/cubes-doo/hosting/master/configs/nginx/sites-available/laravel.conf
+
+sed -i 's/WEBSITE/mydomain.com/' /etc/nginx/sites-available/$DOMAIN.conf
+sed -i 's/www.WEBSITE/www.mydomain.com/' /etc/nginx/sites-available/$DOMAIN.conf
+cd /etc/nginx/sites-enabled
+ln -s ../sites-available/$DOMAIN.conf
+systemctl reload nginx
