ce058cb50381bf63ebe91fa693e105515c9fa08c
MIlan <84564050+mil4ndjcubes@users.noreply.github.com>
2025-02-10 14:38:40 +0100

    Update hostng-standalone.sh

--- install/debian/hostng-standalone.sh
+++ install/debian/hostng-standalone.sh
@@ -7,9 +7,9 @@ systemctl restart sshd
 
 timedatectl set-timezone Europe/Belgrade
 
-apt-get -y install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/debian `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
 apt-get -y update
 apt-get -y install nginx
